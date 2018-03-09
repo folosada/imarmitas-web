e567e34b4154e236adbf552f6406c4cf8bfc5716
Flávio Losada <flavio_gs96@hotmail.com>
2018-03-08 21:17:01 -0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: "http://192.168.0.15:8080",
+  serverUrl: "https://imarmitas-server.herokuapp.com/",
   pathImage: "/restaurante/imagem/"
   
 };
